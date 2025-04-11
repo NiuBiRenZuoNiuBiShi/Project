b7e19d83ef6acdf5800c355959f7776d66c276f0
张义 <zy1772696711@gmail.com>
2025-04-11 18:03:03 +0800

    launch
    # Conflicts:
    #	doc/软件开发计划书.md

---
+++ src/main/java/com/setravel/swifttravel/SwiftTravelApplication.java
@@ -0,0 +1,13 @@
+package com.setravel.swifttravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftTravelApplication.class, args);
+    }
+
+}
