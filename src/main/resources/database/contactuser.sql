a1142e4cd46397cacadf11f4a95184feea06d90d
bastienased <2857499785@qq.com>
2025-04-20 20:23:01 +0800

    new contactuser

---
+++ src/main/resources/database/contactuser.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Auth.Contacts (
+    contact_id BINARY(16) NOT NULL PRIMARY KEY,     
+    user_id BINARY(16) NOT NULL,                 
+    contact_name VARCHAR(100) NOT NULL,             
+    contact_phone VARCHAR(20),                       
+    contact_email VARCHAR(100),                      
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
+    del TINYINT(1) DEFAULT 0                         
+) COMMENT '联系人表';
