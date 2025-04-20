ea966a3d9a6a85ebce5c7f6dac9acb922a56c6d0
郑宇杰 <22241014>
2025-04-20 23:14:16 +0800

    修改酒店预订表

--- src/main/resources/database/HotelReservation.sql
+++ src/main/resources/database/HotelReservation.sql
@@ -1,52 +1,46 @@
--- 用户表
-CREATE TABLE User (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(100),
-    phone VARCHAR(15)
-);
-
 -- 酒店表
 CREATE TABLE Hotel (
-    hotel_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    address VARCHAR(255),
-    rating DECIMAL(2, 1),
-    description TEXT
+    id           BINARY(16)  NOT NULL PRIMARY KEY,
+    name         VARCHAR(50) NOT NULL,
+    address      VARCHAR(50),
+    rating       DECIMAL(2, 1),
+    description  TEXT
+
+    del          TINYINT(1)  NOT NULL DEFAULT 0
 );
 
 -- 房间表
 CREATE TABLE Room (
-    room_id INT AUTO_INCREMENT PRIMARY KEY,
-    hotel_id INT,
-    room_type VARCHAR(50),
-    price DECIMAL(10, 2),
+    id           BINARY(16)  NOT NULL PRIMARY KEY,
+    hotel_id     BINARY(16),
+    room_type    VARCHAR(50),
+    price        DECIMAL(10, 2),
     availability BOOLEAN DEFAULT TRUE,
-    FOREIGN KEY (hotel_id) REFERENCES Hotel(hotel_id)
+
+    del          TINYINT(1)  NOT NULL DEFAULT 0
 );
 
 -- 预订表
 CREATE TABLE Reservation (
-    reservation_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    hotel_id INT,
-    room_id INT,
-    check_in_date DATE,
-    check_out_date DATE,
-    status VARCHAR(50) DEFAULT 'Booked',
-    booking_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES User(user_id),
-    FOREIGN KEY (hotel_id) REFERENCES Hotel(hotel_id),
-    FOREIGN KEY (room_id) REFERENCES Room(room_id)
+    id            BINARY(16)  NOT NULL PRIMARY KEY,
+    user_id       BINARY(16),
+    hotel_id      BINARY(16),
+    room_id       BINARY(16),
+    checkin_date  DATE,
+    checkout_date DATE,
+    status        VARCHAR(50) DEFAULT 'Booked',
+    booking_time  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    del          TINYINT(1)  NOT NULL DEFAULT 0
 );
 
 -- 支付表
 CREATE TABLE Payment (
-    payment_id INT AUTO_INCREMENT PRIMARY KEY,
-    reservation_id INT,
+    id             BINARY(16)  NOT NULL PRIMARY KEY,
+    reservation_id BINARY(16),
     payment_status VARCHAR(50) DEFAULT 'Pending',
-    payment_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    amount DECIMAL(10, 2),
-    FOREIGN KEY (reservation_id) REFERENCES Reservation(reservation_id)
+    payment_time   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    amount         DECIMAL(10, 2),
+
+    del          TINYINT(1)  NOT NULL DEFAULT 0
 );
