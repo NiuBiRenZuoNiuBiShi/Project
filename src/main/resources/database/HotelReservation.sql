993d239602d244f84ce03faf2cf97c16d2be67ff
郑宇杰 <22241014>
2025-04-21 22:35:52 +0800

    hotelres

--- src/main/resources/database/HotelReservation.sql
+++ src/main/resources/database/HotelReservation.sql
@@ -2,10 +2,10 @@
 CREATE TABLE Hotel (
     id           BINARY(16)  NOT NULL PRIMARY KEY,
     name         VARCHAR(50) NOT NULL,
-    address      VARCHAR(50),
+    address      VARCHAR(50) NOT NULL,
     rating       DECIMAL(2, 1),
     description  VARCHAR(255),
-    pic_url      VARCHAR(255)   NOT NULL,
+    pic_url      VARCHAR(255),
 
     del          TINYINT(1)  NOT NULL DEFAULT 0
 );
@@ -13,7 +13,7 @@ CREATE TABLE Hotel (
 -- 房间表
 CREATE TABLE Room (
     id           BINARY(16)  NOT NULL PRIMARY KEY,
-    hotel_id     BINARY(16),
+    hotel_id     BINARY(16)  NOT NULL,
     room_type    VARCHAR(50),
     price        DECIMAL(10, 2),
     room_date    DATE,
