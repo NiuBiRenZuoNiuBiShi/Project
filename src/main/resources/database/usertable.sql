2f8db4eb1e26aa3ac2d542094dc88052074d9cfb
ZhangYi <13887325901@163.com>
2025-04-21 22:13:31 +0800

    Add id_identity in Users

--- src/main/resources/database/usertable.sql
+++ src/main/resources/database/usertable.sql
@@ -3,7 +3,10 @@ CREATE TABLE Users
     id         BINARY(16)   NOT NULL PRIMARY KEY,
     username   VARCHAR(50)  NOT NULL,
     password   VARCHAR(255) NOT NULL,
+    id_card     VARCHAR(20) NOT NULL,
+    phone      VARCHAR(20) NOT NULL,
     email      VARCHAR(100) NOT NULL,
+
     created_at TIMESTAMP  DEFAULT CURRENT_TIMESTAMP,
 
     del        TINYINT(1) DEFAULT 0,
