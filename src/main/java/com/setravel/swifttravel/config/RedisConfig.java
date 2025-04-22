d97fac4b5b5c6f08643d806d6e6d2606b35442a8
ZhangYi <13887325901@163.com>
2025-04-22 19:34:59 +0800

    This is the Beginning of the developments !!!!!

---
+++ src/main/java/com/setravel/swifttravel/config/RedisConfig.java
@@ -0,0 +1,51 @@
+package com.setravel.swifttravel.config;
+
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.time.Duration;
+
+@Configurable
+public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName("10.194.183.100");
+        config.setPort(6379);
+        config.setPassword("");
+        config.setDatabase(0);
+
+        GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+        poolConfig.setMaxWait(Duration.ofMillis(3000));
+        poolConfig.setMaxIdle(8);
+        poolConfig.setMinIdle(4);
+        poolConfig.setMaxTotal(3000);
+
+        LettucePoolingClientConfiguration poolingClientConfig =
+                        LettucePoolingClientConfiguration.builder().
+                        commandTimeout(Duration.ofMillis(3000))
+                        .poolConfig(poolConfig)
+                        .build();
+
+        return new LettuceConnectionFactory(config, poolingClientConfig);
+    }
+
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory());
+        redisTemplate.setDefaultSerializer(
+                new Jackson2JsonRedisSerializer<>(Object.class));
+        redisTemplate.setEnableTransactionSupport(true);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
