9de44191a003a8aab2bb1e2152fa1a659f3c93a3
ZhangYi <13887325901@163.com>
2025-04-27 17:31:17 +0800

    Add Some Comments On the Class **TrainNumberDetail**

--- src/main/java/com/setravel/swifttravel/entities/TrainNumberDetail.java
+++ src/main/java/com/setravel/swifttravel/entities/TrainNumberDetail.java
@@ -24,15 +24,27 @@ public class TrainNumberDetail {
     @JsonFormat(pattern = "HH:mm:ss")
     private List<LocalTime> waitingTimeLine;
 
+    /**
+    *   这些属性的意思是，商务座在第几个车厢，一等座在第几个车厢
+    */
     private List<Integer> business_coach;
     private List<Integer> first_coach;
     private List<Integer> second_coach;
 
+    /**
+    *   这些字段的作用是指名每个座车厢有多少个座位，
+    *   比如说`business_seats_num`的长度和`business_coach`一样
+    *   分别对应每个车厢有多少座位
+     */
     private List<Integer> business_seats_num;
     private List<Integer> first_seats_num;
     private List<Integer> second_seats_num;
     private Integer no_seats_num;
 
+    /**
+    *   这些属性表示价格，长度和`stationLine`一样
+    *   因此，每个属性的第一个值都应该是0
+    * */
     private List<BigDecimal> business_price;
     private List<BigDecimal> first_price;
     private List<BigDecimal> second_price;
