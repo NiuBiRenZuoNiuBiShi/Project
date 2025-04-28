672c10e8a58f86fcbd1e861302bbca521c361bb3
Tengpaz <tengpaz_zhen@outlook.com>
2025-04-28 18:21:27 +0800

    添加查询车票接口

---
+++ src/main/java/com/setravel/swifttravel/trainSale/CarriagesMapper.java
@@ -0,0 +1,9 @@
+package com.setravel.swifttravel.trainSale;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.setravel.swifttravel.entities.Carriages;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CarriagesMapper extends BaseMapper<Carriages> {
+}
---
+++ src/main/java/com/setravel/swifttravel/trainSale/ticketGet.java
@@ -0,0 +1,33 @@
+package com.setravel.swifttravel.trainSale;
+
+import com.setravel.swifttravel.entities.Carriages;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ticketGet {
+
+    @Resource
+    private CarriagesService carriagesService;
+
+    @GetMapping("/getCarriages")
+    public List<Carriages> getCarriages(@RequestParam String depStation, @RequestParam String depCity,
+                                        @RequestParam String arrStation, @RequestParam String arrCity,
+                                        @RequestParam String depDate) {
+        if (depStation != null && arrStation != null) {
+            return carriagesService.getCarriagesBySS(depStation, arrStation, depDate);
+        } else if (depCity != null && arrCity != null) {
+            return carriagesService.getCarriagesByCC(depCity, arrCity, depDate);
+        } else if (depCity != null && arrStation != null) {
+            return carriagesService.getCarriagesByCS(depCity, arrStation, depDate);
+        } else if (depStation != null && arrCity != null) {
+            return carriagesService.getCarriagesBySC(depStation, arrCity, depDate);
+        } else {
+            return null; // or throw an exception
+        }
+    }
+}
