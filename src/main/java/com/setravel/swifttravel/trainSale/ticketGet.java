b453cc8429771acbecb72235a4c06c54f8c85893
Tengpaz <tengpaz_zhen@outlook.com>
2025-05-02 09:45:41 +0800

    略微修改接口

--- src/main/java/com/setravel/swifttravel/trainSale/ticketGet.java
+++ src/main/java/com/setravel/swifttravel/trainSale/ticketGet.java
@@ -3,12 +3,18 @@ package com.setravel.swifttravel.trainSale;
 import com.setravel.swifttravel.entities.Carriages;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * 火车购票
+ * @author Tengpaz
+ */
 @RestController
+@RequestMapping("api/ticket")
 public class ticketGet {
 
     @Resource
