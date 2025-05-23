a0708a3f67472ecd4b73596d5ab821b12c436605
Tengpaz <tengpaz_zhen@outlook.com>
2025-05-23 16:45:06 +0800

    修改了一点配置

--- src/main/java/com/setravel/swifttravel/controller/OrderFoodController.java
+++ src/main/java/com/setravel/swifttravel/controller/OrderFoodController.java
@@ -1,5 +1,10 @@
 package com.setravel.swifttravel.controller;
 
+import com.setravel.swifttravel.entities.Food;
+import com.setravel.swifttravel.entities.Result;
+
+import java.util.List;
+
 public class OrderFoodController {
-    
+
 }
